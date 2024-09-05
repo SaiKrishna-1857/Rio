647ff82e2adad6d66c0ff996dfa29f5b5ed93e52
SAISRIRAM <saisriramkurapati@gmail.com>
2024-09-05 10:11:49 -0400

    updated homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Route as well
 
 const App: React.FC = () => {
   return (
     <Router basename="/chatBot_frontend">
-
       <div className="App">
         <main>
-          <ChatBox />
+          <Routes>
+            {/* Define a default route or any other route for ChatBox */}
+            <Route path="/" element={<ChatBox />} />
+          </Routes>
         </main>
       </div>
     </Router>
