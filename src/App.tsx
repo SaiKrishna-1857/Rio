8fe6d5412d1ae246c646d52c136e9137d5a60adb
SAISRIRAM <saisriramkurapati@gmail.com>
2024-09-05 09:56:31 -0400

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <main>
-        <ChatBox />
-      </main>
-    </div>
+    <Router basename="/personal-portfolio">
+
+      <div className="App">
+        <main>
+          <ChatBox />
+        </main>
+      </div>
+    </Router>
   );
 }
 
